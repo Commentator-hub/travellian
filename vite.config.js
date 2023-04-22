0e2dfc6897438713bba534ec3c8681c1603dc2c9
Ilya Medvedev <medvedokrut@gmail.com>
2023-04-22 18:24:28 +0300

    Init project

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+
+import vitePugPlugin from 'vite-plugin-pug-transformer';
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+
+export default defineConfig({
+    root: root,
+    build: {
+        outDir: outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: resolve(root, 'index.html'),
+                // about: resolve(root, 'about', 'index.html') // Если понадобится добавить несколько страниц.
+            }
+        }
+    },
+    plugins: [vitePugPlugin()]
+})
